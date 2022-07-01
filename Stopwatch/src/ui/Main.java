50562d0fe815365d92d1f12198b4b0349b996379
Jeff-Peters513 <petersjeff288@gmail.com>
2022-07-01 14:45:31 -0400

    init commit stopwatch GUI app

---
+++ Stopwatch/src/ui/Main.java
@@ -0,0 +1,14 @@
+package ui;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// bro code stop watch GUI
+		
+		@SuppressWarnings("unused")
+		Stopwatch stopwatch = new Stopwatch();
+//		@SuppressWarnings("unused")
+//		Stopwatch stopwatch2 = new Stopwatch();
+	}
+
+}
